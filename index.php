93d1015658e8efc99208b4289bb1de28fe9ab13e
Aram Zucker-Scharff <azuckers@gmu.edu>
2011-10-17 00:22:13 -0400

    Adding the existing files.

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<!-- saved from url=(0038)http://clioweb.org/workshops/html-css/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>Newsletter Test</title>
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script src="js/ie7/IE9.js" type="text/javascript"></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+
+		<link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/>
+	<![endif]-->
+	
+
+    </head>
+    
+    <body id="index" class="home">
+        
+		<div id="wrapper">
+			<div id="main" class="body">
+			
+				<header>
+				<div id="title">
+				</div>
+					<hgroup>
+						<h1>Title</h1>
+						<h2>Sub-Title</h2>
+					</hgroup>
+					<aside><article>
+						<div id="action-top">
+						</div>
+					</article></aside>
+					<aside>
+						<div id="subnav">
+						
+							<div id="subnav1" class="actionnav">
+							</div>
+							<div id="subnav2" class="actionnav">
+							</div>
+							<div id="subnav3" class="actionnav">
+							</div>
+							<div id="subnav4" class="actionnav">
+							</div>
+							<div id="subnav5" class="actionnav">
+							</div>
+						
+						</div>
+					</aside>
+				</header>
+				
+				<div class="hfeed">
+				<section id="prev-content" class="body">
+					<div id="content-list">
+						<ol id="previews">
+							<li><article class="hentry">
+								<div class="story-prev">
+									<div class="story-prev-title"><header>
+									</header></div>
+									<div class="prev-caption"><footer>
+									</footer></div>
+								</div>
+							</li>
+							<li><article class="hentry">
+								<div class="story-prev">
+									<div class="story-prev-title"><header>
+									</header></div>
+									<div class="prev-caption"><footer>
+									</footer></div>
+								</div>
+							</li>
+							<li><article class="hentry">
+								<div class="story-prev">
+									<div class="story-prev-title"><header>
+									</header></div>
+									<div class="prev-caption"><footer>
+									</footer></div>
+								</div>
+							</li>
+						</ol>
+					</div>
+				</section>
+				
+				<section id="content" class="body">
+					<div id="content-list">
+						<ol id="previews">
+							<li><article class="hentry">
+								<div class="story-prev">
+									<div class="story-prev-title"><header>
+									</header></div>
+									<div class="prev-caption"><footer>
+									</footer></div>
+								</div>
+							</li>
+							<li><article class="hentry">
+								<div class="story-prev">
+									<div class="story-prev-title"><header>
+									</header></div>
+									<div class="prev-caption"><footer>
+									</footer></div>
+								</div>
+							</li>
+						</ol>
+					</div>
+					<div id="subcontent">
+						<ol id="story-section">
+							<li class="story-med">
+								<article class="hentry">
+									<header><h4 class="story-med-title">Title</h4></header>
+									<div class="story-img">
+									</div>
+									<div class="story-content">
+									</div>
+									<footer>Read more...</footer>
+								</article>
+							</li>
+							<li class="story-med">
+								<article class="hentry">
+									<header><h4 class="story-med-title">Title</h4></header>
+									<div class="story-img">
+									</div>
+									<div class="story-content">
+									</div>
+									<footer>Read more...</footer>
+								</article>
+							</li>
+							<li class="story-med">
+								<article class="hentry">
+									<header><h4 class="story-med-title">Title</h4></header>
+									<div class="story-img">
+									</div>
+									<div class="story-content">
+									</div>
+									<footer>Read more...</footer>
+								</article>
+							</li>
+							<li class="story-med">
+								<article class="hentry">
+									<header><h4 class="story-med-title">Title</h4></header>
+									<div class="story-img">
+									</div>
+									<div class="story-content">
+									</div>
+									<footer>Read more...</footer>
+								</article>
+							</li>
+						</ol>
+					</div>
+					<div id="sidebar" class="sidebar-box">
+					</div>
+				</section>
+				
+				</div>
+			</div>
+		</div>
+	
+</body></html>
