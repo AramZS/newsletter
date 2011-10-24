7400faeccc8f3b03ad6408d80417ec079499e799
Aram Zucker-Scharff <azuckers@gmu.edu>
2011-10-24 00:18:17 -0400

    Changed to table layout.

---
+++ index-t.php
@@ -0,0 +1,439 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="600" border="0" height="120" bgcolor="#91ae3d" cellpadding="0" cellspacing="0" style="color:#ffffff; font-family:Arial, Helvetica, sans-serif;">
+	<tr height="10">
+    	<td height="10"></td>
+    </tr>
+	<tr>
+    	<td width="300"><img src="imgs/ulifeheader.png" width="300" height="100" alt="pursuit" /></td>
+        <td>
+        	<table width="300" cellpadding="0" cellspacing="0" style="font-size:10px; text-align:right;">
+            	<tr height="70">
+                	<td width="100" height="70">
+               	    <img src="imgs/head1.png" width="70" height="70" alt="Story One" />
+                    </td>
+               	  	<td width="100" height="70">
+                    <img src="imgs/head2.png" width="70" height="70" alt="Story Two" />
+                    </td>
+                	<td width="90" height="70">
+                    <img src="imgs/head3.png" width="70" height="70" alt="Story Three" />
+                    </td>
+                    <td width="10" height="70">
+                    </td>                                        
+                </tr>
+                <tr height="30">
+                	<td width="100" height="30">
+                    	Students build connections.
+                    </td>
+                	<td width="100" height="30">
+                    	New employers at the Job Fair.
+                    </td>
+                	<td width="100" height="30">
+                    	EIP reaches new students.
+                    </td>                                        
+                </tr>
+            </table>
+        </td>
+    </tr>
+  	<tr height="10">
+    	<td height="10"></td>
+    </tr>
+</table>
+<table width="600" border="0" cellpadding="0" cellspacing="0" height="20">
+    <tr>
+    	<td bgcolor="#91ae3d" style="color:#ffffff; font-family:Arial, Helvetica, sans-serif;"><img src="imgs/ulifeline.png" width="138" height="20" alt="University Life" /><img src="imgs/newslifeline.png" width="138" height="20" alt="newsletter" /></td>
+        <td width="200" bgcolor="#91ae3d" style="color:#ffffff; font-family:Arial, Helvetica, sans-serif; text-align:right; font-size:12px">Issue 1: October 23, 2011</td>
+    </tr>
+</table>
+<table width="600" cellpadding="0" cellspacing="0" border="0" bgcolor="#ffffff" background="imgs/bg.png" style="color:#22290e; font-family:Arial, Helvetica, sans-serif;">
+  <tr height="10">
+  	<td height="10" colspan="5"></td>
+  </tr>
+  <tr>
+    <td width="10">&nbsp;</td>
+    <td width="380"><h2 style="font-size:16px; font-weight:bold; font-family:arial; line-height:110%;">This week</h2>
+    	<table width="380" cellpadding="3" cellspacing="0" border="0">
+        	<tr height="10">
+            	<td colspan="2" height="10" bgcolor="#723063"></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="50">
+            	<td width="10" bgcolor="#db3b26"></td>
+                <td bgcolor="#91ae3d" width="320"><h1 style="font-size:28px; font-weight:bold; font-family:arial; line-height:110%;">Letter from the VP</h1></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="4">
+            	
+                <td colspan="3" bgcolor="#51b4b6" height="4"></td>
+                
+            </tr>
+            <tr>
+            	
+                <td colspan="3" bgcolor="#51b4b6" style="color:#ffffff; font-size:12px;">
+                	Interview with unit leadership<br />
+                    speaking on new events and increases in yearly reports.
+                </td>
+            </tr>
+            <tr>
+            <td colspan="3" height="10"></td>
+            </tr>
+    	</table>
+        <table cellpadding="0" cellspacing="0" border="0" width="380">
+        	
+        <tr>
+            <td width="150">
+       	    <img src="imgs/head.png" width="150" height="150" alt="Head" />
+            </td>
+            <td width="10">
+            </td>
+            <td valign="top" style="font-size:14px;">
+            	<br  />
+            	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            	<table width="220" cellpadding="0" cellspacing="0" border="0">
+                	<tr>
+                    	<td height="10">
+                        	
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td width="160">
+                        
+                        </td>
+                        <td align="right" bgcolor="#91ae3d" height="50" >
+                        	Listen&nbsp;&nbsp;&nbsp;
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr height="50">
+        	<td height="50">
+            </td>
+        </tr>
+        </table>
+        
+    	<table width="380" cellpadding="3" cellspacing="0" border="0">
+        	<tr height="10">
+            	<td colspan="2" height="10" bgcolor="#723063"></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="50">
+            	<td width="10" bgcolor="#db3b26"></td>
+                <td bgcolor="#91ae3d" width="320"><h1 style="font-size:28px; font-weight:bold; font-family:arial; line-height:110%;">Our core values</h1></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="4">
+            	
+                <td colspan="3" bgcolor="#51b4b6" height="4"></td>
+                
+            </tr>
+            <tr>
+            	
+                <td colspan="3" bgcolor="#51b4b6" style="color:#ffffff; font-size:12px;">
+                	Interview with unit leadership<br />
+                    speaking on new events and increases in yearly reports.
+                </td>
+            </tr>
+            <tr>
+            <td colspan="3" height="10"></td>
+            </tr>
+    	</table>
+        <table cellpadding="0" cellspacing="0" border="0" width="380">
+        	
+        <tr>
+            <td width="150">
+       	    <img src="imgs/0001.jpg" width="150" height="150" alt="Head" />
+            </td>
+            <td width="10">
+            </td>
+            <td valign="top" style="font-size:14px;">
+            	<br  />
+            	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            	<table width="220" cellpadding="0" cellspacing="0" border="0">
+                	<tr>
+                    	<td height="10">
+                        	
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td width="160">
+                        
+                        </td>
+                        <td align="right" bgcolor="#91ae3d" height="50" >
+                        	Watch&nbsp;&nbsp;&nbsp;
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr height="50">
+        	<td height="50">
+            </td>
+        </tr>
+        </table>
+        
+        
+        
+    	<table width="380" cellpadding="3" cellspacing="0" border="0">
+        	<tr height="10">
+            	<td colspan="2" height="10" bgcolor="#723063"></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="50">
+            	<td width="10" bgcolor="#db3b26"></td>
+                <td bgcolor="#91ae3d" width="320"><h1 style="font-size:28px; font-weight:bold; font-family:arial; line-height:110%;">Professional development</h1></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="4">
+            	
+                <td colspan="3" bgcolor="#51b4b6" height="4"></td>
+                
+            </tr>
+            <tr>
+            	
+                <td colspan="3" bgcolor="#51b4b6" style="color:#ffffff; font-size:12px;">
+                	Interview with unit leadership<br />
+                    speaking on new events and increases in yearly reports.
+                </td>
+            </tr>
+            <tr>
+            <td colspan="3" height="10"></td>
+            </tr>
+    	</table>
+        <table cellpadding="0" cellspacing="0" border="0" width="380">
+        	
+        <tr>
+            <td width="150">
+       	    <img src="imgs/jacques.jpg" width="150" height="150" alt="Head" />
+            </td>
+            <td width="10">
+            </td>
+            <td valign="top" style="font-size:14px;">
+            	<br  />
+            	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            	<table width="220" cellpadding="0" cellspacing="0" border="0">
+                	<tr>
+                    	<td height="10">
+                        	
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td width="160">
+                        
+                        </td>
+                        <td align="right" bgcolor="#91ae3d" height="50" >
+                        	Read&nbsp;&nbsp;&nbsp;
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr height="50">
+        	<td height="50">
+            </td>
+        </tr>
+        </table>       
+        
+        
+    	<table width="380" cellpadding="3" cellspacing="0" border="0">
+        	<tr height="10">
+            	<td colspan="2" height="10" bgcolor="#723063"></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="50">
+            	<td width="10" bgcolor="#db3b26"></td>
+                <td bgcolor="#91ae3d" width="320"><h1 style="font-size:28px; font-weight:bold; font-family:arial; line-height:110%;">Letter from the editor</h1></td>
+                <td bgcolor="#51b4b6"></td>
+            </tr>
+            <tr height="4">
+            	
+                <td colspan="3" bgcolor="#51b4b6" height="4"></td>
+                
+            </tr>
+            <tr>
+            	
+                <td colspan="3" bgcolor="#51b4b6" style="color:#ffffff; font-size:12px;">
+                	Interview with unit leadership<br />
+                    speaking on new events and increases in yearly reports.
+                </td>
+            </tr>
+            <tr>
+            <td colspan="3" height="10"></td>
+            </tr>
+    	</table>
+        <table cellpadding="0" cellspacing="0" border="0" width="380">
+        	
+        <tr>
+            <td width="150">
+       	    <img src="imgs/aram.jpg" width="150" height="150" alt="Head" />
+            </td>
+            <td width="10">
+            </td>
+            <td valign="top" style="font-size:14px;">
+            	<br  />
+            	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            	<table width="220" cellpadding="0" cellspacing="0" border="0">
+                	<tr>
+                    	<td height="10">
+                        	
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td width="160">
+                        
+                        </td>
+                        <td align="right" bgcolor="#91ae3d" height="50" >Read&nbsp;&nbsp;&nbsp;
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr height="50">
+        	<td height="50">
+            </td>
+        </tr>
+        </table>
+        
+                        
+    </td>
+    <td width="20" background="imgs/bg-vert.gif">&nbsp;</td>
+    <td width="180" valign="top">
+    	<table cellpadding="0" cellspacing="0" border="0" width="180">
+        	<tr>
+            	<td valign="top">
+                	<h2 style="font-size:20px; font-weight:bold; font-family:arial; line-height:110%;">Upcoming Events</h2>
+                </td>
+            </tr>
+            <tr>
+            	<td height="10"></td>
+            </tr>
+            <tr>
+            	<td valign="top" style="font-size:12px">
+                <ul>
+            	  <li>
+           	      <a href="http://today.gmu.edu/56902/" style="color:#360">HIV/AIDS Awareness Month</a><br />
+            	  All this month.</li><br />
+            	  <li><a href="http://today.gmu.edu/57870/" style="color:#360">Mason Fall Premiere</a><br />
+            	    Get involved today!
+            	  </li><br />
+                  <li>
+           	      <a href="http://today.gmu.edu/57328/" style="color:#360">Prince William Little Theater: One Flew Over the Cuckoo's Nest</a><br />
+            	  Great new show.</li><br />
+                  <li>
+           	      <a href="http://today.gmu.edu/57956/" style="color:#360">Immigration Film Series: Sin Pais and Papers</a><br />
+            	  Documentary films.</li><br />
+          	    </ul>
+              </td>
+            </tr>
+            <tr>
+            	<td height="20">
+                	
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<h2 style="font-size:20px; font-weight:bold; font-family:arial; line-height:110%;">New Faces</h2>
+                </td>
+            </tr>
+            <tr>
+            	<td height="10">
+                	
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<table cellpadding="0" cellspacing="0" border="0" style="font-size:12px;">
+                    	<tr>
+                        	<td>
+                            	<img src="imgs/0001.jpg" width="80" height="80" alt="Head" />
+                            </td>
+                            <td width="10">
+                            </td>
+                            <td >
+                            Meet Todd Rose, our new Associate Dean. 
+                            </td>
+                        </tr>
+                        <tr>
+                        	<td colspan="3" height="6">
+                            </td>
+                        </tr>
+                        <tr>
+                        	<td>
+                            	<img src="imgs/aram.jpg" width="80" height="80" alt="Head" />
+                            </td>
+                            <td width="10">
+                            </td>
+                            <td style="font-size:10px;">
+                            	Meet Aram Zucker-Scharff, our new Community Manager</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+            	<td height="20">
+                	
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<table>
+                    	<tr>
+                        	<td>
+                            	<img src="imgs/twittericon.png" width="32" height="32" alt="Head" />
+                            </td>
+                            <td><h2 style="font-size:20px; font-weight:bold; font-family:arial; line-height:110%;">&nbsp;Highlights</h2></td>
+                       </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+            	<td height="10">
+                	
+                </td>
+            </tr>
+            <tr>
+            	<td style="font-size:14px;">
+                	Great new information about events on some stuff.<br /><br />
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br /><br />
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </td>
+            </tr>
+            <tr>
+            	<td height="20">
+                	
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<h2 style="font-size:20px; font-weight:bold; font-family:arial; line-height:110%;">Tweetables</h2>
+                </td>
+            </tr>
+            <tr>
+            	<td height="10">
+                	
+                </td>
+            </tr>   
+            <tr>
+            	<td style="font-size:12px;">
+                	Great info on University Life in 140 characters or less. Fill your feed!
+                    <br /><br />
+                    Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                    <br /><br />
+                    Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                    <br /><br />
+                </td>
+            </tr>         
+        </table>
+    </td>
+    <td width="10">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
